33cc5d78293492a4276589b81bfe6f5b345e7a4e
henrydatei <henrydatei@web.de>
2019-12-16 12:09:01 +0100

    Update list_of_games.sh

--- list_of_games.sh
+++ list_of_games.sh
@@ -8,7 +8,10 @@ state=$(cat games.txt | jq ".user.games | .[$i].opponent")
 while [ "$state" != "null" ]; do
 	name=$(echo "$state" | jq -r ".name")
 	gameID=$(cat games.txt | jq ".user.games | .[$i].game_id")
-	echo "Spiel gegen \033[32m$name\033[0m mit der game_id \033[32m$gameID\033[0m"
+	currentRound=$(cat games.txt | jq ".user.games | .[$i].cat_choices | length")
+	yourPoints=$(cat games.txt | jq ".user.games | .[$i].your_answers" | grep "0" | wc -l | bc)
+	opponentPoints=$(cat games.txt | jq ".user.games | .[$i].opponent_answers" | grep "0" | wc -l | bc)
+	echo "Spiel gegen \033[32m$name\033[0m mit der game_id \033[32m$gameID\033[0m ($yourPoints:$opponentPoints [Runde $currentRound])"
 	i=$(echo "$i+1" | bc)
 	state=$(cat games.txt | jq ".user.games | .[$i].opponent")
 done
