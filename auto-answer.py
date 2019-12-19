b57065e2ff9ddc4ceccaf1390dbb5c87823c1c6b
henrydatei <henrydatei@web.de>
2019-12-19 10:39:31 +0100

    Create auto-answer.py

---
+++ auto-answer.py
@@ -0,0 +1,39 @@
+import quizduell
+import cookielib
+import json
+import os
+import argparse
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Give me a gameID!')
+parser.add_argument("--numberOfZeros")
+parser.add_argument("--gameID")
+
+args = parser.parse_args()
+numberOfZeros = args.numberOfZeros
+gameID = args.gameID
+
+# Load authenticated session from file to prevent unnecessary logins:
+cookie_jar = cookielib.MozillaCookieJar('cookie_file')
+api = quizduell.QuizduellApi(cookie_jar)
+
+if os.access(cookie_jar.filename, os.F_OK):
+    cookie_jar.load()
+else:
+    api.login_user('henrydatei', 'henrydatei')
+
+api = quizduell.QuizduellApi(cookie_jar)
+answerArray = np.array([0])
+for i in range(int(numberOfZeros) - 1):
+    answerArray = np.append(answerArray, 0)
+result = api.upload_round_answers(gameID, answerArray, 1)
+
+if 'access' in result:
+    # Session invalid, re-login:
+    api.login_user('henrydatei', 'henrydatei')
+    result = api.top_list_rating()
+
+# Store authenticated session in file:
+cookie_jar.save()
+
+print json.dumps(result, sort_keys=True, indent=4)
