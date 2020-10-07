f7b78040f759ecc1d197ae6a0428de3105be4329
henrydatei <31316025+henrydatei@users.noreply.github.com>
2020-10-07 16:12:23 +0200

    higher percentages for the next 630 ranks

--- legit-bot.sh
+++ legit-bot.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # successrates
-sportUndFreizeit=0.85					# cat_id = 11
-essenUndTrinken=0.85					# cat_id = 3
-glaubeUndReligion=0.85				# cat_id = 4
-kinofilme=0.85								# cat_id = 16
-wunderDerTechnik=0.85					# cat_id = 1
-draussenImGruenen=0.85				# cat_id = 9
-computerspiele=0.85						# cat_id = 14
-zeugenDerZeit=0.85						# cat_id = 6
-buecherUndWoerter=0.85				# cat_id = 2
-koerperUndGeist=0.85					# cat_id = 10
-rundUmDieWelt=0.85						# cat_id = 5
-machtUndGeld=0.85							# cat_id = 13
-medienUndUnterhaltung=0.85		# cat_id = 19
-kunstUndKultur=0.85						# cat_id = 20
-tvSerien=0.85									# cat_id = 17
-imLabor=0.85									# cat_id = 7
-comics=0.85										# cat_id = 8
-die2000er=0.85								# cat_id = 15
-defaultrate=0.85
+sportUndFreizeit=0.95				# cat_id = 11
+essenUndTrinken=0.95				# cat_id = 3
+glaubeUndReligion=0.95				# cat_id = 4
+kinofilme=0.95					# cat_id = 16
+wunderDerTechnik=0.95				# cat_id = 1
+draussenImGruenen=0.95				# cat_id = 9
+computerspiele=0.95				# cat_id = 14
+zeugenDerZeit=0.95				# cat_id = 6
+buecherUndWoerter=0.95				# cat_id = 2
+koerperUndGeist=0.95				# cat_id = 10
+rundUmDieWelt=0.95				# cat_id = 5
+machtUndGeld=0.95				# cat_id = 13
+medienUndUnterhaltung=0.95			# cat_id = 19
+kunstUndKultur=0.95				# cat_id = 20
+tvSerien=0.95					# cat_id = 17
+imLabor=0.95					# cat_id = 7
+comics=0.95					# cat_id = 8
+die2000er=0.95					# cat_id = 15
+defaultrate=0.95
 
 if [ -f account.txt ]; then
   username=$(cat account.txt | cut -d "|" -f1)
